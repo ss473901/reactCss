7ee1d1988ae80b5da3964f4c44131daeda49aa69
眞田悟司 <84009331+ss473901@users.noreply.github.com>
2022-01-19 00:19:17 +0900

    Initial commit

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,28 @@
+export const InlineStyle = () => {
+  const containerStyele = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItemns: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "#3d84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
+  return (
+    <div style={containerStyele}>
+      <p style={titleStyle}>- Inline Styles -</p>
+      <button style={buttonStyle}>FIGHT!!</button>
+    </div>
+  );
+};
